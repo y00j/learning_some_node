1f7e7b2a5511697d3582d30ee98361a878bbb740
Yujie Zhu <yujiezhu718@gmail.com>
2018-06-24 23:17:29 -0700

    finished getMovieTitles function

--- index2.js
+++ index2.js
@@ -1,23 +1,24 @@
 const https = require('https');
 
-let movieTitles = [];
-let numPages = 1;
+
+let numPages = 2;
 let firstTime = true;
 
 async function getMovieTitles(substr) {
   let page = 1;
-  while (numPages > 0) {
-    await queryData(substr, page);
-    console.log('queryData finished for page = ' + page + ' numPages =' + numPages);
-    //also update numPages after first request : TODO
+  let movieTitles = [];
+  while (page <= numPages) {
+    let movies = await queryData(substr, page);
+    movies.forEach((movie) => {
+      movieTitles.push(movie.Title);
+    });
     page++;
-    numPages--;
-    console.log("after decrementing numPages", numPages)
+
   }
   return movieTitles;
 }
 
-const queryData = async (substr, pageNum) => {
+const queryData = (substr, pageNum) => {
 
 
   return new Promise((yes, no) => {
@@ -27,25 +28,21 @@ const queryData = async (substr, pageNum) => {
     https.get(url, (resp) => { 
       let movieData = '';
 
-        // A chunk of data has been recieved. 
+
       resp.on('data', (chunk) => {  
         movieData += chunk; 
       });
 
-        // The whole response has been received. Print out the result. 
+
       resp.on('end', () => {  
         movieData = JSON.parse(movieData);
         let movies = movieData.data;
         if (firstTime) {
           numPages = movieData.total_pages;
-          console.log("firstTime", numPages);
           firstTime = false;
         }
-        movies.forEach((movie) => {
-          console.log(movie.Title);
-          movieTitles.push(movie.Title);
-        });
-        yes();
+
+        yes(movies);
       });
     });
   });
@@ -54,48 +51,9 @@ const queryData = async (substr, pageNum) => {
 
 
 
-// https.get(url, (res) => {
-//   let data = '';
-//   res.on('data', (chunk) => {
-//     data += chunk;
-//     data = JSON.parse(data);
 
-//     movies = data.data;
-//     totalPages = data.total_pages;
-//     movies.forEach((movie) => {
-//       movieTitles.push(movie.Title);
-//     });
-
-//     // console.log(movieTitles);
-
-
-//     for (let i = 2; i <= totalPages; i++) {
-//       let nextUrl = `${url}&page=${i}`;
-//       https.get(nextUrl, (nextRes) => {
-//         nextRes.on('data', (nextChunk) => {
-//           let nextMovies = JSON.parse(nextChunk).data;
-//           nextMovies.forEach((movie) => {
-//             movieTitles.push(movie.Title);
-//           });
-//           console.log(movieTitles, "this is loggin")
-//         });
-
-//       });
-//     }
-
-
-//     // getMovies().then(() => console.log(movieTitles, "new movie titles"));
-
-//   });
-
-// });
-
-
-
-var movieTitles2;
-
-getMovieTitles("man")
+getMovieTitles("spiderman")
   .then((m) => {
     movieTitles2 = m;
-    console.log(movieTitles2, movieTitles.length);
+    console.log(movieTitles2, movieTitles2.length);
   });
